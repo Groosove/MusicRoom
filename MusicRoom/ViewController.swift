6772c11777493c33fe2d9aedc3a6f4515033cc16
Artur Lutfullin <arturianec2013@gmail.com>
2022-02-19 22:39:28 +0300

    Start Project

---
+++ MusicRoom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicRoom
+//
+//  Created by Артур Лутфуллин on 19.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
